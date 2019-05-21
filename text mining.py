ea71031324e498f9b11d7beee3ee45f1c0552243
ArielKomen <ariel.komen@filternet.nl>
2019-05-21 10:05:06 +0200

    actuele versie

--- text mining.py
+++ text mining.py
@@ -38,7 +38,7 @@ def pubmed_zoeken(d2_lijst):
     lijst_met_records = []
     opteller = 0
     sleep_time = 0
-    export_lijst = []
+    
     lijst_met_getallen = []
     tweede_lijst_met_getallen = []
     
@@ -49,7 +49,7 @@ def pubmed_zoeken(d2_lijst):
 
     Entrez.email = "A.komen@student.han.nl"
 
-    for zoekterm in lijst_met_ziektes_compounds[0:]:
+    for zoekterm in lijst_met_bitter_gourd_ziektes[0:]:
         count = get_count(zoekterm)
         time.sleep(sleep_time)
         
@@ -60,16 +60,21 @@ def pubmed_zoeken(d2_lijst):
         if int(count) > 0:
             lijst_met_records = get_artikelen(zoekterm, count, lijst_met_records)
             time.sleep(sleep_time)
+            print("zoveel hits: "+str(count)+"en bij deze term: "+ zoekterm)
         else:
             print("deze zoekterm heeft geen hits: "+zoekterm)
     
     #print("zoveel hits in totaal hebben meer dan 10000 artikelen: "+str(opteller))
     visualiseer_aantal_artikelen(lijst_met_getallen, tweede_lijst_met_getallen)
-<<<<<<< master
-=======
-    opteller = 0
->>>>>>> Ariel zijn troep
+
+    # maak de export lijst.
+    export_lijst = make_export_lijst(lijst_met_records, lijst_met_bitter_gourd_ziektes)
+
+    return export_lijst
     
+def make_export_lijst(lijst_met_records, lijst_met_ziektes_compound):
+    export_lijst = []
+    opteller = 0
     # in deze forloop de data in een bestandje zetten zodat je het later weer kan gebruiken ergens anders voor.
     for record in lijst_met_records:
         export_lijst = get_info(record["IdList"], export_lijst, lijst_met_ziektes_compounds[opteller])
@@ -85,11 +90,10 @@ def visualiseer_aantal_artikelen(lijst_met_getallen, tweede_lijst_met_getallen):
     
     if len(lijst) == len(tweede_lijst_met_getallen):
         plt.scatter(tweede_lijst_met_getallen,lijst)
-<<<<<<< master
-        plt.yticks(np.arange(min(lijst), max(lijst)+1, 100.0))
-=======
+
         plt.yticks(np.arange(min(lijst), max(lijst)+1, 1000.0))
->>>>>>> Ariel zijn troep
+
+        plt.yticks(np.arange(min(lijst), max(lijst)+1, 100.0))
         plt.xlabel("verschillende termen")
         plt.ylabel("hoeveelheid gevonden artikelen")
         plt.title("hoeveelheid artikelen met termen")
@@ -163,7 +167,7 @@ def get_info(idlist, export_lijst, bitter_gourd_ziekte):
 
 def schrijf_data_weg(export_lijst):
     # in deze methode de data wegschrijven naar een tekstbestandje
-    bestand = open("/home/cole/Documents/course_8/weektaken/textmining applicatie/data/export ziektes en compounds", "a")
+    bestand = open("/home/cole/Documents/course_8/weektaken/textmining applicatie/data/export", "a")
 
     for lijst in export_lijst:
         regel = lijst[0]+";"+lijst[1]+";"+lijst[2]+";"+lijst[3]+";"+lijst[4]+"\n"

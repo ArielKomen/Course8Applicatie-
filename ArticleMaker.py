8d6ecea93c1035039d09243d58f8fc8a5774c816
Stephan Gui <stephan.gui4@gmail.com>
2019-06-04 21:18:45 +0200

    shits geupdate

--- ArticleMaker.py
+++ ArticleMaker.py
@@ -6,7 +6,6 @@ from Article_class import Article
 def main():
     article_list, disease_dictionary, compound_dictionary = article_maker()
     JSON_maker(article_list, disease_dictionary, compound_dictionary)
-    #db_filler(article_list, disease_dictionary)
 def article_maker():
     article_list = []
     unique_disease = []
@@ -104,13 +103,4 @@ def JSON_maker(article_list, disease_dictionary, compound_dictionary):
         json.dump(Bittergourd_compound_disease_JSON, outfile2)
         outfile.close()
 
-def db_filler(article_list, disease_dictionary):
-
-    cnx = mysql.connector.connect(host = "hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com", user = 'owe7_pg8@hannl-hlo-bioinformatica-mysqlsrv', password = 'blaat1234', database = 'owe7_pg8')
-    cursor = cnx.cursor()
-    for object in article_list:
-        query_insert = "insert into owe7_pg8.article values(%s, %s, %s, %s, %s)"
-        values = (int(object.get_pmid(object)), object.get_date(), object.get_title(), 255, disease_dictionary.get(object.get_ziekte(object)))
-        print(values)
-        #cursor.execute(query_insert, values)
 main()
